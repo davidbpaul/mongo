afc4d95f3f30fb78d339b56e95b5f820fcec714b
David Paul <david-paul23@hotmail.com>
2016-09-19 20:59:02 +0000

    re

--- express_server.js
+++ express_server.js
@@ -9,45 +9,25 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 const PORT = process.env.PORT || 8080; // default port 8080
-
-
 app.use(methodOverride('_method'));
+//server
 const assert = require('assert');
 const MongoClient = require("mongodb").MongoClient;
 const objectID = require("mongodb").ObjectID;
 const MONGODB_URI = process.env.MONGODB_URI
-
 console.log(`Connecting to MongoDB running at: ${MONGODB_URI}`);
-let collection = null;
-
-MongoClient.connect(MONGODB_URI, (err, db) => {
-
-  if (err) {
-    console.log('Could not connect! Unexpected error. Details below.');
-    throw err;
-  }
-
-  console.log('Connected to the database!');
-  collection = db.collection("urls");
-
-});
-
-
 
 
 app.get("/", (req, res) => {
-  res.end("Hello!");
+res.redirect(`http://localhost:8080/urls/new`);
 });
-
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-
-
-
 app.get("/urls", (req, res) => {
   MongoClient.connect(MONGODB_URI, (err, db) => {
+    let collection = db.collection("urls");
     collection.find().toArray((err, result) => {
       let longURL = result;
       let templateVars = {
@@ -57,32 +37,32 @@ app.get("/urls", (req, res) => {
       console.log(templateVars)
       res.render("urls_index", templateVars);
     })
-      // console.log(result)
-  })
+    })
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-
 app.get("/urls/:shortURL", (req, res) => {
   MongoClient.connect(MONGODB_URI, (err, db) => {
-  getLongURL(req.params.shortURL, (err,  longURL)=>{
-    let templateVars = {
-      shortURL:req.params.shortURL,
-      longURL:longURL
-    };
-    res.render("urls_show", templateVars);
+    let collection = db.collection("urls");
+    getLongURL(req.params.shortURL, (err,  longURL)=>{
+      let templateVars = {
+        shortURL:req.params.shortURL,
+        longURL:longURL
+      };
+      res.render("urls_show", templateVars);
    // console.log(longURL)
-  });
-});
+    });
+ });
 });
 
 
 
 app.post("/urls", (req, res, next) => {
   MongoClient.connect(MONGODB_URI, (err, db) => {
+    let collection = db.collection("urls");
     let random =Math.random().toString(36).substr(2, 6);
 
     let item ={
@@ -101,6 +81,7 @@ app.post("/urls", (req, res, next) => {
 
 app.get("/u/:shortURL", (req, res) => {
   MongoClient.connect(MONGODB_URI, (err, db) => {
+     let collection = db.collection("urls");
   res.redirect(req.body.longURL);
 });
 });
@@ -111,21 +92,25 @@ app.listen(PORT, () => {
 
 
 app.delete("/urls/:shortURL", (req, res)=>{
-  let shortURL = req.body.shortURL;
-  collection.deleteOne({"shortURL": req.params.shortURL},function (err, result) {
-    console.log('item deleted');
-    res.redirect("/urls");
+   MongoClient.connect(MONGODB_URI, (err, db) => {
+     let shortURL = req.body.shortURL;
+     let collection = db.collection("urls");
+     collection.deleteOne({"shortURL": req.params.shortURL},function (err, result) {
+       console.log('item deleted');
+       res.redirect("/urls");
   })
-
+})
 })
 
 app.put("/urls/:shortURL", (req, res)=>{
-let random =Math.random().toString(36).substr(2, 6);
-  let shortURL = req.body.shortURL
-  let item ={
-    longURL :req.body.longURL,
-    shortURL:random
-  };
+   MongoClient.connect(MONGODB_URI, (err, db) => {
+     let collection = db.collection("urls");
+     let random =Math.random().toString(36).substr(2, 6);
+     let shortURL = req.body.shortURL
+     let item ={
+        longURL :req.body.longURL,
+        shortURL:random
+      };
 
   collection.updateOne({"shortURL": req.params.shortURL}, {$set: item}, function (err, result) {
 
@@ -133,12 +118,10 @@ let random =Math.random().toString(36).substr(2, 6);
     res.redirect(`http://localhost:8080/urls/`);
   })
 })
-
-
-
+ })
 function getLongURL(shortURL, cb) {
   MongoClient.connect(MONGODB_URI, (err, db) => {
-
+    let collection = db.collection("urls");
     if (err) {
       console.log('Could not connect! Unexpected error. Details below.');
       throw err;
@@ -153,14 +136,3 @@ function getLongURL(shortURL, cb) {
   });
 
 }
-
-function withdb(cb){
-   MongoClient.connect(MONGODB_URI, (err, db) => {
-
-    if (err) {
-      console.log('Could not connect! Unexpected error. Details below.');
-      throw err;
-    }
-})
-}
-
